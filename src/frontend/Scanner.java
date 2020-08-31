9bb4fd68cfeabd482c89a5db010bf8fc68571bdf
abdurrahmanmohammad <58004923+abdurrahmanmohammad@users.noreply.github.com>
2020-08-31 13:40:33 -0700

    Skips comments similar to skipping whitespaces. Reformatted code using Eclipse.

--- src/frontend/Scanner.java
+++ src/frontend/Scanner.java
@@ -7,48 +7,45 @@
  */
 package frontend;
 
-public class Scanner
-{
-    private Source source;
-    private boolean inComment; //whether the scanner is currently in a comment
-    
-    /**
-     * Constructor.
-     * @param source the input source.
-     */
-    public Scanner(Source source)
-    {
-        this.source = source;
-    }
-    
-    public boolean isWhitespace(char ch)
-    {
-    	if(ch == '{')
-    		inComment = true;
-    	
-    	else if(inComment && ch == '}')
-    	{
-    		inComment = false;
-    		return true; //no longer in a comment, but the character still counts as whitespace
-    	}
-    	
-    	return Character.isWhitespace(ch) || inComment;
-    }
-    
-    /**
-     * Extract the next token from the source.
-     * @return the token.
-     */
-    public Token nextToken()
-    {
-        char ch = source.currentChar();
-        
-        // Skip blanks and other whitespace characters.
-        while (isWhitespace(ch)) ch = source.nextChar();
+public class Scanner {
+	private Source source;
 
-        if (Character.isLetter(ch))     return Token.word(ch, source);
-        else if (Character.isDigit(ch)) return Token.number(ch, source);
-        else if (ch == '\'')            return Token.string(ch, source);
-        else                            return Token.specialSymbol(ch, source);
-    }
+	/**
+	 * Constructor.
+	 * 
+	 * @param source the input source.
+	 */
+	public Scanner(Source source) {
+		this.source = source;
+	}
+
+	/**
+	 * Extract the next token from the source.
+	 * 
+	 * @return the token.
+	 */
+	public Token nextToken() {
+		char ch = source.currentChar();
+
+		// Skip blanks and other whitespace characters.
+		while (Character.isWhitespace(ch))
+			ch = source.nextChar();
+		
+		// Skip Comments
+		if(ch == '{')
+			while(ch != '}') ch = source.nextChar();
+		
+		
+
+		if (Character.isLetter(ch)) // A-Z & a-z
+			return Token.word(ch, source);
+		else if (Character.isDigit(ch)) // 0-9
+			return Token.number(ch, source);
+		else if (ch == '\'') // '
+			return Token.string(ch, source);
+		else { // Special Symbols
+			return Token.specialSymbol(ch, source);
+			
+		}
+	}
 }
